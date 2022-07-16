3ca6a5de3041c4db885ca4c8f6c64261343cde2d
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-07-16 12:17:12 +0100

    Add homework

---
+++ TextProcessing/replaceRepeatingChars.js
@@ -0,0 +1,11 @@
+function replaceRepeatingChars(input) {
+  let res = input[0];
+
+  for (let i = 1; i < input.length; i++) {
+    if (input[i] !== input[i - 1]) {
+      res += input[i];
+    }
+  }
+  console.log(res);
+}
+replaceRepeatingChars("aaaaabbbbbcdddeeeedssaa");
