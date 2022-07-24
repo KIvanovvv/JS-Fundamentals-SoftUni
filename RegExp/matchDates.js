18ce6a7748131320d6095ce4c0962024041cc9dc
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-07-24 12:48:34 +0100

    Add homework

---
+++ RegExp/matchDates.js
@@ -0,0 +1,17 @@
+function matchDates(dates) {
+  let pattern =
+    /(?<day>\d{2})([-|.|\/])(?<month>[A-Z]{1}[a-z]{2})\2(?<year>\d{4})/g;
+  let text = dates.join(" ");
+
+  let valid;
+
+  while ((valid = pattern.exec(text)) !== null) {
+    let day = valid.groups.day;
+    let month = valid.groups.month;
+    let year = valid.groups.year;
+    console.log(`Day: ${day}, Month: ${month}, Year: ${year}`);
+  }
+}
+matchDates([
+  "13/Jul/1928, 10-Nov-1934, , 01/Jan-1951,f 25.Dec.1937 23/09/1973,1/Feb/2016",
+]);
